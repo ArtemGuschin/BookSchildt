f24bb74dcb28d456b7c0181b1c5b048d875e7d3a
art69 <@ArtemGuschin>
2024-09-14 19:03:20 +0300

    first commit

--- src/Artem/javacore/chapter10/Exc2.java
+++ src/Artem/javacore/chapter10/Exc2.java
@@ -1,2 +1,16 @@
-package Artem.javacore.chapter10;public class Exc2 {
+package Artem.javacore.chapter10;
+
+public class Exc2 {
+    public static void main(String[] args) {
+        int a, d;
+        try {
+            d = 0;
+            a = 42 / d;
+            System.out.println("Это не будет выведен");
+        }catch (ArithmeticException e ){
+            System.out.println("Деление на нуль ");
+        }
+        System.out.println("После оператора catch");
+
+    }
 }
---
+++ src/Artem/javacore/chapter10/SuperSubCatch.java
@@ -0,0 +1,15 @@
+package Artem.javacore.chapter10;
+
+public class SuperSubCatch {
+    public static void main(String[] args) {
+        try {
+            int a = 0;
+            int b = 42 / a;
+
+        }catch (ArithmeticException e ){
+            System.out.println("Деление на нуль");
+        }catch (Exception e){
+            System.out.println("Перехват тсключений общего класса Exception  ");
+        }
+    }
+}
