8f0764cf5e958624400892220cd49069cac0af80
art69 <@ArtemGuschin>
2024-09-23 18:36:32 +0300

    add chapter 29

---
+++ src/main/java/com/artem/javacore/chapter29/StreamDemo3.java
@@ -0,0 +1,22 @@
+package com.artem.javacore.chapter29;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamDemo3 {
+    public static void main(String[] args) {
+        List<Double> myList = new ArrayList<>();
+        myList.add(7.0);
+        myList.add(18.0);
+        myList.add(10.0);
+        myList.add(24.0);
+        myList.add(17.0);
+        myList.add(5.0);
+        double productOfSrqRoots = myList.parallelStream().reduce(
+                1.0,
+                (a, b) -> a * Math.sqrt(b),
+                (a, b) -> a * b
+        );
+        System.out.println("Произведение квадпвтных корней " + productOfSrqRoots);
+    }
+}
